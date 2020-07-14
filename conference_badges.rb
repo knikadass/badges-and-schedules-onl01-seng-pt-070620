ab4be6406e416351f7397e6dd2c8d96c0492150e
Knika Dass <knikadass@gmail.com>
2020-07-14 20:35:21 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -19,9 +19,10 @@ def assign_rooms(attendees)
   rooms 
 end
 
-#def printer(attendees)
-#batch_badge_creator(attendees).each do |badges|
-#end
-
-#assign_rooms(attendees).each do |rooms|
-#end
+def printer(attendees)
+batch_badge_creator(attendees).each do |badges|
+  puts badges
+end
+ assign_rooms(attendees).each do |rooms|
+   puts rooms
+end
